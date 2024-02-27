6902b82ee8ddf61383382e8c47bdaaa49005e4e5
Jonathan Galliengit config --global user.name Jonathan <lj.gallien@gmail.com>
2024-02-27 03:10:03 -0500

    typecheck, lint, prettier pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState<number>(0);
-  const lightColors: String[] = ['red', 'yellow', 'green']
+  const lightColors: string[] = ['red', 'yellow', 'green'];
 
   function changeLight() {
-    setCount((count + 1) % lightColors.length)
+    const newCount = count === 0 ? lightColors.length - 1 : count - 1;
+    setCount(newCount);
   }
 
   return (
     <>
-      <div onClick={changeLight} className='stoplight-text'><h1>Click to Change light color.</h1></div>
+      <div onClick={changeLight} className="stoplight-text">
+        <h1>Click to Change light color.</h1>
+      </div>
       <div className="light-container">
         {lightColors.map((color, index) => (
-            <div key={index} className={index === count ? 'light active ' + color : 'light ' + color}></div>
+          <div
+            key={index}
+            className={
+              index === count ? 'light active ' + color : 'light ' + color
+            }
+          ></div>
         ))}
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
